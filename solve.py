ece66be1bc641356eb62f33b11fad4112fd1519b
oknick <63999887+oknick@users.noreply.github.com>
2021-11-14 02:42:39 +0800

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,4 +1,5 @@
 from pwn import *
+# https://storage.googleapis.com/ctfd-uploads/filtered.tar.gz_9a6cb1b3eafce70ff549ba6b942f34a9.gz
 ## p = remote("chalp.hkcert21.pwnable.hk",28028)
 p = process("a.out")
 p.recvuntil("service.")
